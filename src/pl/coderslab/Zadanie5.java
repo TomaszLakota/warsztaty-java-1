1707efb25d389d6ac271c438d46dbfd9905ba9d6
Tomasz Łakota <48764424+TomaszLakota@users.noreply.github.com>
2020-03-22 12:59:28 +0100

    complete task 5

--- src/pl/coderslab/Zadanie5.java
+++ src/pl/coderslab/Zadanie5.java
@@ -7,24 +7,78 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Zadanie5 {
 
-    public static void playGame(){
-        Connection connect = Jsoup.connect("http://www.onet.pl/");
+
+    public static void main(String[] args) {
+        Path popularWordsPath = Paths.get("popular_words.txt");
+        Path filteredWordsPath = Paths.get("filtered_words.txt");
+
+        List<String> popularWords = getWords("http://www.onet.pl/");
+
         try {
-            Document document = connect.get();
-            Elements links = document.select("span.title");
-            for (Element elem : links) {
-                System.out.println(elem.text());
+            Files.write(popularWordsPath, popularWords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // -----------------
+
+        popularWords.clear();
+        try {
+            for (String line : Files.readAllLines(popularWordsPath)) {
+                popularWords.add(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+
+        List<String> filteredWords = new ArrayList<>();
+        String ignoredWords = "oraz poniewaz albo dlatego";
+        List<String> filter = new ArrayList<>();
+        for (String word : ignoredWords.split(" ")) {
+            filter.add(word);
+        }
+        for (String word : popularWords) {
+            if (!filter.contains(word)) {
+                filteredWords.add(word);
+            }
+        }
+
+        try {
+            Files.write(filteredWordsPath, filteredWords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("done");
     }
-    public static void main(String[] args) {
-	// write your code here
-        playGame();
+
+    static List<String> getWords(String url) {
+        List<String> words = new ArrayList<>();
+        Connection connect = Jsoup.connect(url);
+        try {
+            Document document = connect.get();
+            Elements titles = document.select("span.title");
+            for (Element e : titles) {
+                String title = e.text().replaceAll("[^a-zA-Z0-9 ąćżźółęśń]", "");
+                String[] titleArr = title.split(" ");
+                for (String word : titleArr) {
+                    if (word.length() > 3 && !words.contains(word)) {
+                        words.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
     }
+
+
 }
